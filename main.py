e4499c780383562e320d174b1220f9732a68e0ef
park4352 <144448237+park4352@users.noreply.github.com>
2023-09-22 20:51:57 +0900

    Add files via upload
    퀴즈 파일

---
+++ main.py
@@ -0,0 +1,21 @@
+point = [90, 45, 64, 9, 17, 29]
+results = []
+
+for i in point :
+
+    if i >= 71:
+        a = "A"
+        results.append(a)
+    elif i >=41:
+       b = "B"
+       results.append(b)
+    elif i >=11:
+       c = "c'"
+       results.append(c)
+    elif i >=10:
+        d = "D"
+        results.append(d)
+
+
+print(results)
+
